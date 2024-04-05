c8c7851f487724e65b094ff5d80df7044cd999ee
Alexandre Esposte <alexandreesposte@gmail.com>
2024-04-05 16:40:39 -0300

    salvando gravação, é necessário ajustes

--- scr/continuo.py
+++ scr/continuo.py
@@ -7,11 +7,12 @@ import seriais
 import threading
 import time
 import queue
+import os
 
 
 
 serial1, serial2, serial3, bancada = seriais.NumerosSeriais()
-camera = cam.Camera(bancada,exposicao=60000, ganho = 10, fps=200)
+camera = cam.Camera(bancada,exposicao=70000, ganho = 20, fps=200)
 camera.conectar()
 
 
@@ -20,19 +21,26 @@ thread1.start()
 
 
 cv2.namedWindow("Frame", cv2.WINDOW_NORMAL)
-cv2.resizeWindow('1', 1216, 1936)
+cv2.resizeWindow('Frame', 1280, 720)
 
 data = []
+output_dir = 'frames'
+i = 0
 while True:
     try:
+
+
    
         img = camera.array
         #img= cv2.applyColorMap(camera.array, cv2.COLORMAP_PLASMA)
-        
-        #print(np.mean(img))
+        resized_frame = cv2.resize(img, (1000, 1000))
+        frame = cv2.cvtColor(resized_frame, cv2.COLOR_BGR2RGB)
+        cv2.imwrite(os.path.join(output_dir, f'frame_{i:04d}.png'), frame)
+
         data.append(round(np.mean(img),2))
-        print(camera.fps_aquisitado,camera.fps_transferido)
+        print(np.mean(img))
         cv2.imshow('Frame',img)
+        i+=1
         
         if cv2.waitKey(1) & 0xFF == ord('q'):
             camera.movie=False
@@ -46,6 +54,10 @@ while True:
         print('Medida falhou')
 
 cv2.destroyAllWindows()
+os.system(f'ffmpeg -r 30 -i frames/frame_%04d.png -c:v libx265 -vf fps=30 -crf 28 -preset slow -tune fastdecode -pix_fmt yuv420p -s 1280x720 -movflags +faststart output.mp4')
+for file_name in os.listdir(output_dir):
+    file_path = os.path.join(output_dir, file_name)
+    os.remove(file_path)
 
 plt.plot([x for x in range(len(data))],data)
 plt.show()
